5ba32f74316ecfd72d3f7ef9850f047ee1f3780b
Jacufier <jacufier@gmail.com>
2015-04-15 04:59:33 +0800

    Update Falldown.js

--- Falldown.js
+++ Falldown.js
@@ -1,11 +1,15 @@
 #pragma strict
-var Speed = 0.5;
-function Start () {
 
+var Speed : float;
+
+function Start () {
+	var Speed = 0.5
 }
+
 function OnCollisionEnter(collision:Collision) {
 		this.transform.Translate(Vector3(1,0,0)*Speed*Time.deltaTime);
 	}
+	
 function Update () {
 
 }
